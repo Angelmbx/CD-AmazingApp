7cafe6dd089a721147545bc7f244d3541fee3add
angel <jjjk>
2023-09-13 13:53:19 +0200

    Dao Interface created

---
+++ src/main/java/com/campusdual/appamazing/model/dao/IProductDao.java
@@ -0,0 +1,10 @@
+package com.campusdual.appamazing.model.dao;
+
+import com.campusdual.appamazing.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IProductDao extends JpaRepository<Product, Integer> {
+
+
+
+}
