9245255fd486a34ee72515900dc522e7c23f50fb
angel <jjjk>
2023-09-14 09:36:16 +0200

    wip Services with expanations

--- src/main/java/com/campusdual/appamazing/service/ProductService.java
+++ src/main/java/com/campusdual/appamazing/service/ProductService.java
@@ -1,8 +1,10 @@
 package com.campusdual.appamazing.service;
 
 import com.campusdual.appamazing.api.IProductService;
+import com.campusdual.appamazing.model.Product;
 import com.campusdual.appamazing.model.dao.IProductDao;
 import com.campusdual.appamazing.model.dto.ProductDTO;
+import com.campusdual.appamazing.model.dto.dtoMapper.ProductMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
@@ -18,7 +20,15 @@ public class ProductService implements IProductService {
 
     @Override
     public ProductDTO queryProduct(ProductDTO productDTO) {
-        return null;
+        //DAO no puede trabajar con DTOs solo con entidades que hay en la BD
+        //Lo primero que hacemos entonces es una conversión
+        Product product = ProductMapper.INSTANCE.toEntity(productDTO); //esto se podría omitir porque el DTO tambien tiene id... pero la conversion del return es necesaria igualmente
+        //ahora ya tenemos nuestro Producto,
+
+
+        //le paso un Id de un producto, me devuelve la tupla correspondiente
+        //vuelvo a hacer la conversión
+        return ProductMapper.INSTANCE.toDTO(productDao.getReferenceById(product.getId()));
     }
 
     @Override
