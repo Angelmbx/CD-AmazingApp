f81fd81efe63c8b7fdb66e7cb070afe841fc2be0
angel <jjjk>
2023-09-14 12:55:42 +0200

    Working on controller

--- src/main/java/com/campusdual/appamazing/controller/ProductController.java
+++ src/main/java/com/campusdual/appamazing/controller/ProductController.java
@@ -1,6 +1,11 @@
 package com.campusdual.appamazing.controller;
 
+import com.campusdual.appamazing.api.IProductService;
+import com.campusdual.appamazing.model.dto.ProductDTO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController() //indicamos que es el controlador
 @RequestMapping("/products") //para acceder a productos > http://localhost:3030/products
@@ -12,10 +17,34 @@ public class ProductController {
         return "viva Vigo"; //si introduces solo http://localhost:3030/products print este String
     }*/
 
+    @Autowired
+    private IProductService productService;
+
     @PostMapping
     public String testController(@RequestBody String name){
-        return "Product controller works " + name + "!!"; // lo mismo pero pasandole un JSON con un String por Postman
+        return "Product controller works " + name + "!!"; // lo mismo pero pasandole un JSON con un String por Postman haciendo un Post
+    }               //sirve para controlar que funcione el controlador
+
+    @PostMapping("/get") //como no puede haber 2 sin argumentos ponemos /get por ejemplo
+    // ahora para que ejecute esta query la ruta será: http://localhost:3030/products/get
+    public ProductDTO queryProduct(@RequestBody ProductDTO productDTO){
+       return this.productService.queryProduct(productDTO);
     }
+
+    @GetMapping("/get/{id}")
+    public ProductDTO queryProduct(@PathVariable int id){ //get sin pasarle JSON. > http://localhost:30030/products/get/1 mismo resultado. cambias num cambia id
+        ProductDTO productDTO = new ProductDTO();
+        productDTO.setId(id);
+        return this.productService.queryProduct(productDTO);
+    }
+
+    @GetMapping("/getAll")
+    public List<ProductDTO> queryAllProducts(){
+        return this.productService.queryAllProducts();
+    }
+    //hasta aquí lo gets.
+
+    //ahora insert, update, delete.
 
 
 }
